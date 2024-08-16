1c8c79c0abf87771efb07979d4bd162edccee571
Mohammad Broughani <gitmbroughani81@gmail.com>
2024-08-16 12:11:52 +0330

    ~

--- wsk-commands.sh
+++ wsk-commands.sh
@@ -2,6 +2,6 @@ wsk action update clj-get target/openwhisk-app-0.1.0-SNAPSHOT-standalone.jar --m
 
 wsk action update clj-add target/openwhisk-app-0.1.0-SNAPSHOT-standalone.jar --main io.github.mbroughani81.register_user.Main --kind java:8
 
-wsk action invoke clj-add --param username mbroughani81 --param password pass1pass2  --result
+#wsk action invoke clj-add --param username mbroughani81 --param password pass1pass2  --result
 
-wsk action invoke clj-get --result
+#wsk action invoke clj-get --result
