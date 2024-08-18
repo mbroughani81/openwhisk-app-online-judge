17d5cc449b5a0b587fb2b43629932f534ec487ae
Mohammad Broughani <gitmbroughani81@gmail.com>
2024-08-18 10:14:32 +0330

    complete thing

--- src/io/github/mbroughani81/eval_code.clj
+++ src/io/github/mbroughani81/eval_code.clj
@@ -10,46 +10,70 @@
 
 ;; ------------------------------------------------------------ ;;
 
-(def cnt (atom 0))
-
-;; ------------------------------------------------------------ ;;
-
-(defn main [{:keys [code]
+(defn main [{:keys [code param]
              :as   args}]
-  (swap! cnt inc)
-  (println "GOOZ")
   (when (not (deref system/system-started?))
     (reset! system/system-started? true)
     (swap! system/system
            (fn [-system-]
              (component/start -system-))))
-  (let [url   "http://10.10.0.1:3233/api/v1/namespaces/guest/actions/rest-action?overwrite=true"
-        -cnt- (deref cnt)]
-    (client/put url
-                {:body         (-> {:exec {:kind "nodejs:default"
-                                           :code code}}
-                                   (cheshire.core/generate-string))
-                 :content-type :json
-                 :basic-auth   ["23bc46b1-71f6-4ed5-8c54-816aa4f8c502"
-                                "123zO3xZCLrMN6v2BKK1dXYFpXlPkccOFqm12CdAsMgRU4VrNZ9lyGVCGuMDGIwP"]}))
-  (-> {:result "ok!"}))
+  (let [create-action-url "http://10.10.0.1:3233/api/v1/namespaces/guest/actions/rest-action?overwrite=true"
+        invoke-action-url "http://10.10.0.1:3233/api/v1/namespaces/guest/actions/rest-action?blocking=true"
+        delete-action-url "http://10.10.0.1:3233/api/v1/namespaces/guest/actions/rest-action?blocking=true"
+        auth              ["23bc46b1-71f6-4ed5-8c54-816aa4f8c502"
+                           "123zO3xZCLrMN6v2BKK1dXYFpXlPkccOFqm12CdAsMgRU4VrNZ9lyGVCGuMDGIwP"]
+        _                 (client/put create-action-url
+                                      {:body         (-> {:exec {:kind "nodejs:default"
+                                                                 :code code}}
+                                                         (cheshire.core/generate-string))
+                                       :content-type :json
+                                       :basic-auth   auth})
+        response          (client/post invoke-action-url
+                                       {:body         (-> param
+                                                          (cheshire.core/generate-string))
+                                        :content-type :json
+                                        :basic-auth   auth})
+        result            (-> response
+                              :body
+                              (cheshire.core/parse-string keyword)
+                              :response
+                              :result)
+        _                 (client/delete create-action-url
+                                         {:body         (-> {:exec {:kind "nodejs:default"
+                                                                    :code code}}
+                                                            (cheshire.core/generate-string))
+                                          :content-type :json
+                                          :basic-auth   auth})]
+    (-> {:result result})))
 
 (utils/OpenWhisk-Action-Entry "io.github.mbroughani81.eval_code.Main")
 (utils/OpenWhisk-Main main)
 
 (comment
-  (main {:code "function main(params) { return { message: 1 + 1 }; }"})
+  (main {:code  "function main(params) { return { greeting: `Hello, ${params.name}!` }; }"
+         :param {:name "jafar sexi"}})
 
   (system/reset-system)
 
-  (let [url  "http://localhost:3233/api/v1/namespaces/guest/actions/rest-action?overwrite=true"
-        code "function main(params) { return { message: \"boob-e-sara\" }; }"]
-    (client/put url
-                {:body         (-> {:exec {:kind "nodejs:default"
-                                           :code code}}
-                                   (cheshire.core/generate-string))
-                 :content-type :json
-                 :basic-auth   ["23bc46b1-71f6-4ed5-8c54-816aa4f8c502" "123zO3xZCLrMN6v2BKK1dXYFpXlPkccOFqm12CdAsMgRU4VrNZ9lyGVCGuMDGIwP"]}))
+  (let [url-1    "http://localhost:3233/api/v1/namespaces/guest/actions/rest-action?overwrite=true"
+        code     "function main(params) { return { greeting: `Hello, ${params.name}!` }; }"
+        result-1 (client/put url-1
+                             {:body         (-> {:exec {:kind "nodejs:default"
+                                                        :code code}}
+                                                (cheshire.core/generate-string))
+                              :content-type :json
+                              :basic-auth   ["23bc46b1-71f6-4ed5-8c54-816aa4f8c502" "123zO3xZCLrMN6v2BKK1dXYFpXlPkccOFqm12CdAsMgRU4VrNZ9lyGVCGuMDGIwP"]})
+        body-1   (-> result-1 :body (cheshire.core/parse-string))
+        url-2    "http://localhost:3233/api/v1/namespaces/guest/actions/rest-action?blocking=true"
+        result-2 (client/post url-2
+                              {:body         (-> {:name "jafar"}
+                                                 (cheshire.core/generate-string))
+                               :content-type :json
+                               :basic-auth   ["23bc46b1-71f6-4ed5-8c54-816aa4f8c502" "123zO3xZCLrMN6v2BKK1dXYFpXlPkccOFqm12CdAsMgRU4VrNZ9lyGVCGuMDGIwP"]})
+        body-2   (-> result-2 :body (cheshire.core/parse-string keyword) :response :result)]
+    ;; (clojure.pprint/pprint body)
+    (clojure.pprint/pprint body-1)
+    (clojure.pprint/pprint body-2))
 
-  ;;
+;;
   )
