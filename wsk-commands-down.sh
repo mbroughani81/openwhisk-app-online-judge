d358437533b2d16c9960872726d91e0b06e7ad14
Mohammad Broughani <gitmbroughani81@gmail.com>
2024-09-07 09:53:50 +0330

    Updating bash scripts

--- wsk-commands-down.sh
+++ wsk-commands-down.sh
@@ -2,6 +2,6 @@ wsk action delete clj-get
 wsk action delete clj-register 
 wsk action delete clj-login 
 wsk action delete clj-eval-code
-
-#wsk action invoke clj-register --param username mbroughani81 --param password pass1pass2  --result
+wsk action delete clj-create-problem
+wsk action delete clj-create-submit
 
--- wsk-commands-up.sh
+++ wsk-commands-up.sh
@@ -1,18 +1,7 @@
 wsk action update clj-get target/openwhisk-app-0.1.0-SNAPSHOT-standalone.jar --main io.github.mbroughani81.get_users.Main --kind java:8 --memory 512
-
 wsk action update clj-register target/openwhisk-app-0.1.0-SNAPSHOT-standalone.jar --main io.github.mbroughani81.register_user.Main --kind java:8 --memory 512
-
 wsk action update clj-login target/openwhisk-app-0.1.0-SNAPSHOT-standalone.jar --main io.github.mbroughani81.login_user.Main --kind java:8 --memory 512
-
 wsk action update clj-eval-code target/openwhisk-app-0.1.0-SNAPSHOT-standalone.jar --main io.github.mbroughani81.eval_code.Main --kind java:8 --memory 512
-
-
 wsk action update clj-create-problem target/openwhisk-app-0.1.0-SNAPSHOT-standalone.jar --main io.github.mbroughani81.create_problem.Main --kind java:8 --memory 512
-
 wsk action update clj-create-submit target/openwhisk-app-0.1.0-SNAPSHOT-standalone.jar --main io.github.mbroughani81.create_submit.Main --kind java:8 --memory 512
-
-
-#wsk action invoke clj-get --result
-
-#wsk action invoke clj-register --param username mbroughani81 --param password pass1pass2  --result
 
